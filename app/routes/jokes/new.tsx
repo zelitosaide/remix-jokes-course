7882f71191cca55feb77bcd4c4220107374f55eb
zelitosaide <zelitosaide83@gmail.com>
2022-12-03 17:01:14 +0200

    first commit

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,31 @@
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own hilarious jokes</p>
+      <form method="post">
+        <div>
+          <label>
+            Name:{" "}
+            <input
+              type="text"
+              name="name"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="button"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
