6d35eba5453bab7d8f9b6e1765eaf47ba356b58f
zelitosaide <zelitosaide83@gmail.com>
2022-12-03 23:33:53 +0200

    styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
 
@@ -7,5 +8,20 @@ export const links: LinksFunction = () => {
 };
 
 export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
